2227c5bae6f1aafffd631870dbb333dcfb2003c9
mattvip <matt@victoryipconsulting.com>
2026-04-27 19:47:19 -0500

    Refactor NewOrderPage component layout and styles

--- src/app/orders/new/page.tsx
+++ src/app/orders/new/page.tsx
@@ -1,20 +1,11 @@
-'use client'
-
-import { useEffect } from 'react'
+'use client';
 
 export default function NewOrderPage() {
-  useEffect(() => {
-    alert("NEW ORDER PAGE IS LIVE " + Math.random())
-  }, []);
-
   return (
-    <main>
+    <main style={{padding: 48, background: '#fffbe5'}}>
       <h1>PAGE TEST: /orders/new {Math.random()}</h1>
-      <p style={{fontWeight: 'bold', color: 'red'}}>If you do not see THIS LINE, you are not editing the correct file!</p>
-
-      {/* Place your full order form here */}
-      <p>
-        This is the /orders/new test page. You should only see THIS header and alert if you're editing <b>src/app/orders/new/page.tsx</b>!
+      <p style={{fontWeight: 'bold', color: 'red'}}>
+        If you do not see THIS LINE, you are not editing the correct file!
       </p>
     </main>
   );

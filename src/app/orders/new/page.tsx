aacc56793a32e65cc65dd54e25ece2e8e1746d5d
mattvip <matt@victoryipconsulting.com>
2026-04-27 20:04:36 -0500

    Clean up NewOrderPage component
    Removed unnecessary comments and test content from the new order page.

--- src/app/orders/new/page.tsx
+++ src/app/orders/new/page.tsx
@@ -5,17 +5,12 @@ import { useEffect } from 'react'
 export default function NewOrderPage() {
   useEffect(() => {
     alert("NEW ORDER PAGE IS LIVE " + Math.random())
-  }, []);
+  }, [])
 
   return (
     <main>
       <h1>PAGE TEST: /orders/new {Math.random()}</h1>
       <p style={{fontWeight: 'bold', color: 'red'}}>If you do not see THIS LINE, you are not editing the correct file!</p>
-
-      {/* Place your full order form here */}
-      <p>
-        This is the /orders/new test page. You should only see THIS header and alert if you're editing <b>src/app/orders/new/page.tsx</b>!
-      </p>
     </main>
-  );
+  )
 }
